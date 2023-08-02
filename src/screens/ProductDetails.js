4d5dc88df59ed2f63a028e837c8cbc396d880900
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-08-01 17:12:34 -0700

    imag added to produvt details screen

--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -3,9 +3,31 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { SimpleLineIcons, AntDesign } from '@expo/vector-icons';
 import Details from '../../assets/images/details.png'
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Foundation } from '@expo/vector-icons';
+import {
+  useFonts,
+  Poppins_100Thin,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins'
 
 
 const ProductDetails = () => {
+  let [fontsLoaded] = useFonts({
+    Poppins_100Thin,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -19,12 +41,47 @@ const ProductDetails = () => {
         </View>
 
         <View style={styles.contentContainer}>
-          <Text>Rice</Text>
+          <View>
+            <View style={{ flexDirection: "row", gap: 10, alignItems: "center", justifyContent: "center"}}>
+              <Text style={styles.food}>Rice</Text>
+              <FontAwesome5 name="dot-circle" size={15} color="#2CBC35" style={{flex: 1}} />
+              <Text>Add</Text>
+            </View>
+            <Text style={{ fontWeight: 700, color: "#09B44D", fontSize: 20}}>$100</Text>
+          </View>
+
+          <View>
+            <Text>Recipe</Text>
+            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementc more..........</Text>
+
+            <View>
+              <View style={{ flexDirection: "row", gap: 30, marginBottom: 15}}>
+                <View style={{  borderWidth: 1, width: 60,  height: 40, justifyContent: 'center', alignItems: "center" }}>
+                  <Foundation name="heart" size={24} color="black" />
+                </View>
+                <View>
+                    <Text>ProductDetails</Text>
+                    <Text>Lorem ipsum dolor sit amr</Text>
+                </View>
+              </View>
+
+              <View style={{ flexDirection: "row", gap: 30}}>
+                <View style={{  borderWidth: 1, width: 60,  height: 40, justifyContent: 'center', alignItems: "center" }}>
+                  <Foundation name="heart" size={24} color="black" />
+                </View>
+                <View>
+                    <Text>ProductDetails</Text>
+                    <Text>Lorem ipsum dolor sit amr</Text>
+                </View>
+              </View>
+            </View>
+
+
+          </View>
+
+          
+          
         </View>
-
-
-
-
 
       </View>
     </SafeAreaView>
@@ -53,7 +110,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20
   },
   contentContainer: {
-    
+    paddingHorizontal: 20,
+    paddingTop: 40,
+  },
+  food: {
+    color: "rgba(0, 0, 0, 0.50)",
+    fontSize: 25,
+    fontWeight: 700,
+    fontFamily: ""
   }
   
     
