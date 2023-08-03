efb4bacf4a90b887c8d596d6874e4e8f9e7ecaf2
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-08-03 01:32:17 -0700

    Fix: signup screen fixed

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native'
+import { View, Text, StyleSheet, Image, Dimensions, KeyboardAvoidingView, SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
 import food from '../../assets/images/good.png'
 // import logo from '../../assets/images/logo.png'
@@ -55,7 +55,8 @@ const SignupScreen = () => {
 
     
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView>
     <View>
        <Image resizeMode="contain" style={styles.imgStyle} source={food} />
        {/* <Image resizeMode="contain" style={styles.imgStyle} source={logo} /> */}
@@ -63,7 +64,7 @@ const SignupScreen = () => {
     </View>
 
 
-    <View style={[styles.alignCenter, { flex: 1}]}>
+    <View style={[styles.alignCenter]}>
         <View style={{width: width - 50 }}>
             <View style={{ marginVertical: 10 }}>
                 <Text style={styles.inputLabel}>Email Address</Text>
@@ -100,7 +101,7 @@ const SignupScreen = () => {
     </View>
 
 
-    <View style={styles.alignCenter}>
+    <View style={[styles.alignCenter, {marginTop: 150}]}>
       <CustomButton  text="Create an account" />
     </View>
 
@@ -112,7 +113,8 @@ const SignupScreen = () => {
             <Text style={{ color: "#2CBC35"}}>Sign In</Text>
         </TouchableOpacity>
     </View>
- </View>
+    </KeyboardAvoidingView>
+ </SafeAreaView>
   )
 }
 
@@ -136,7 +138,8 @@ const styles = StyleSheet.create({
         // height: 45
     },
     largeText: {
-        marginTop: 30,
+        marginTop: 10,
+        marginBottom: 25,
         fontSize: 30,
         fontWeight: 'bold',
         fontFamily: "Poppins_600SemiBold",
@@ -163,6 +166,135 @@ const styles = StyleSheet.create({
 })
 
 export default SignupScreen
+
+
+
+
+// import React from 'react'
+// import { View, Text, TouchableOpacity, Dimensions, Image, StyleSheet, TextInput } from 'react-native'
+// import CustomButton from '../components/CustomButton'
+// import logo from '../../assets/images/logo.png'
+
+// const { height, width } = Dimensions.get('screen')
+
+// const SignupScreen = ({ navigation }) => {
+//     return (
+//         <View style={styles.container} >
+//             <View>
+//                 <Image resizeMode="contain" style={styles.imgStyle} source={logo} />
+//                 <Text style={[styles.largeText, { marginTop : 20 }]} > {'Sign up \n now'} </Text>
+//             </View>
+//             <View style={styles.alignCenter} >
+//                 <View style={{ width: width - 50 }} >
+//                     <View style={{ marginVertical: 10 }} >
+//                         <Text style={styles.textInputLabel} > Email Address </Text>
+//                         <TextInput keyboardType="email-address" style={styles.textInput} placeholder="Enter email" />
+//                     </View>
+
+//                     <View style={{ marginVertical: 10 }} >
+//                         <Text style={styles.textInputLabel} > Password </Text>
+//                         <TextInput secureTextEntry style={styles.textInput} placeholder="Enter email" />
+//                     </View>
+
+//                     <View style={{ marginVertical: 10 }} >
+//                         <Text style={styles.textInputLabel} > Confirm Password </Text>
+//                         <TextInput secureTextEntry style={styles.textInput} placeholder="Enter email" />
+//                     </View>
+//                 </View>
+//             </View>
+//             <View style={styles.alignCenter} >
+//                 <TouchableOpacity style={{ marginBottom: 30 }} onPress={() => console.warn('Google Login Not Implemented!!!')} >
+                    
+//                     <View style={[styles.googleBtn, styles.alignCenter, {flexDirection : "row"}]} >
+//                      <Image style={styles.imgIcon} resizeMode={'contain'} source={logo} />
+                        
+//                         <Text> Sign up with Google </Text>
+//                     </View>
+//                 </TouchableOpacity>
+
+//                 <CustomButton buttonHandler={() => console.warn("Home screen not implemented")} text="Create an account" />
+
+//                 <View style={styles.rowFlex} >
+//                     <Text> Already have an account? </Text>
+
+//                     <TouchableOpacity onPress={() => navigation.navigate('login')} >
+//                         <Text style={{ color: "#2CBC35" }} > Sign In </Text>
+//                     </TouchableOpacity>
+//                 </View>
+//             </View>
+//         </View>
+//     )
+// }
+
+// export default SignupScreen
+
+// const styles = StyleSheet.create({
+//     rowFlex: {
+//         flexDirection: 'row'
+//     },
+//     imgIcon: {
+//         height: 25,
+//         width: 25,
+//         marginRight : 10
+//     },
+//     textInputLabel: {
+//         fontSize: 13,
+//         marginBottom: 10,
+//         marginLeft: 10
+//     },
+//     textInput: {
+//         backgroundColor: '#fff',
+//         borderColor: '#DFE2E5',
+//         borderWidth: 1,
+//         height: 45,
+//         paddingLeft: 15,
+//         borderRadius: 15
+//     },
+//     largeText: {
+//         fontSize: 30,
+//         fontWeight: '600'
+//     },
+//     title: {
+//         fontWeight: '700',
+//         fontSize: 22,
+//         paddingVertical: 10
+//     },
+//     text: {
+//         fontSize: 14
+//     },
+//     container: {
+//         height,
+//         flex: 1,
+//         flexDirection: 'column',
+//         justifyContent: 'space-between',
+//         paddingVertical: 20,
+//         paddingHorizontal: 20
+//     },
+//     imgStyle: {
+//         height: 100,
+//         width: 150
+//     },
+//     alignCenter: {
+//         justifyContent: 'center',
+//         alignItems: 'center'
+//     },
+//     googleBtn: {
+//         height: 45,
+//         width: 200,
+//         borderRadius: 20,
+//         borderColor: '#fff',
+//         backgroundColor: '#fff',
+//         borderWidth: 1
+//     },
+//     button: {
+//         height: 45,
+//         width: 300,
+//         borderRadius: 20,
+//         borderColor: '#2CBC35',
+//         backgroundColor: '#2CBC35',
+//         borderWidth: 1
+//     }
+// })
 
 
 
